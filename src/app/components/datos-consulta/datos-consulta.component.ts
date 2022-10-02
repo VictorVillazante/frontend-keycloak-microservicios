9df3d502619acee32b25e34101a24b8ed79fb784
VictorVillazante <villazante.bernabe.victor@gmail.com>
2022-10-02 16:55:39 -0400

    agregar detalle de consulta

--- src/app/components/datos-consulta/datos-consulta.component.ts
+++ src/app/components/datos-consulta/datos-consulta.component.ts
@@ -4,6 +4,7 @@ import { EstadosConsultas } from 'src/app/models/EstadosConsultas';
 import { RecetasService } from 'src/app/services/recetas.service';
 import { EstadosReservaService } from '../../services/estados-reserva.service';
 import { Recetas } from '../../models/Recetas';
+import { ConsultaService } from 'src/app/services/consulta.service';
 
 @Component({
   selector: 'app-datos-consulta',
@@ -13,9 +14,11 @@ import { Recetas } from '../../models/Recetas';
 export class DatosConsultaComponent implements OnInit {
 
   listadoEstados:EstadosConsultas[]=[];
-  constructor(private activatedRoute:ActivatedRoute,private estadosReservaService:EstadosReservaService,private recetasService:RecetasService) { }
+  constructor(private activatedRoute:ActivatedRoute,private consultaService:ConsultaService,private estadosReservaService:EstadosReservaService,private recetasService:RecetasService) { }
   idConsulta:any;
   listadoRecetas:Recetas[]=[];
+  estado_elegido:any;
+  descripcion_consulta:any;
   ngOnInit(): void {
     this.activatedRoute.params.subscribe( params => {
       console.log(params['id'])
@@ -39,6 +42,14 @@ export class DatosConsultaComponent implements OnInit {
       this.obtenerRecetasPorIdConsulta();
     })
   }
-
+  registrarCambios(){
+    let datos={
+      "id_estado_consulta":this.estado_elegido,
+      "informe_consulta":this.descripcion_consulta
+    }
+    console.log(datos);
+    this.consultaService.actualizarConsulta(this.idConsulta,datos).subscribe((data:any)=>{
+    })
+  }
 
 }
